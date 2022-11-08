30ed50db3c690218e78fb85409e8d55af02fab8c
Angel Zabaleta Torrez <94147766+angelzabaleta22@users.noreply.github.com>
2022-11-08 15:00:59 -0500

    04-AdvancePromises

--- 04-AdvancePromises/homework/src/pledge.js
+++ 04-AdvancePromises/homework/src/pledge.js
@@ -1,21 +1,56 @@
-'use strict';
-/*----------------------------------------------------------------
-Promises Workshop: construye la libreria de ES6 promises, pledge.js
-----------------------------------------------------------------*/
-// // TU CÓDIGO AQUÍ:
 
-
-
-module.exports = $Promise;
-/*-------------------------------------------------------
-El spec fue diseñado para funcionar con Test'Em, por lo tanto no necesitamos
-realmente usar module.exports. Pero aquí está para referencia:
-
-module.exports = $Promise;
-
-Entonces en proyectos Node podemos esribir cosas como estas:
-
-var Promise = require('pledge');
-…
-var promise = new Promise(function (resolve, reject) { … });
---------------------------------------------------------*/
+let numMentiras = 10;
+let lasMentirasDeMiEx = new Promise((resolve, reject)=>{
+    if (numMentiras < 20) resolve('Soy un Ganador!')
+    else reject('Soy un perdedor!')
+});
+​
+​
+lasMentirasDeMiEx.then(
+    //CB RESOLVE
+    (result)=>{
+        console.log('RESOLVED_A', result);
+        return result + ' pero no de su corazon!';
+    },
+    //CB REJECT
+    (err)=>{console.log('REJECT_A', err);}
+)
+//NUEVA PROMESA
+.then(
+    //CB RESOLVE
+    (result)=>{
+        console.log('RESOLVED_B', result);
+        //nombre del beatle
+        //buscamos el archivo del beatle
+        //si lo encontramos (return beatle)
+        //si no lo encontramos (throw err)
+        numMentiras = 50;
+        // return 'No me rechazes'
+        // return new Promise((resolve, reject)=>{
+        //     if (numMentiras < 20) resolve('Soy un Ganador!')
+        //     else reject('Soy un perdedor!')
+        // });
+        throw result;
+    },
+    //CB REJECT
+    (err)=>{console.log('REJECT_B', err);}
+)
+//NUEVA PROMESA
+.then(
+    //CB RESOLVE
+    (result)=>{
+        console.log('RESOLVED_C', result);
+        return result + ' pero no de su corazon!';
+    },
+    //CB REJECT
+    // (err)=>{console.log('REJECT_C', err);}
+)
+//NUEVA PROMESA
+.then(
+    //CB RESOLVE
+    (result)=>{
+        console.log('RESOLVED_D', result);
+        return result + ' pero no de su corazon!';
+    }
+)
+.catch((err)=>console.log(err))
